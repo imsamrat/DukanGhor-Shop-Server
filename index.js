def854d329cc3d6f7fac824bac065ebe6940f812
S.m.Samrat <imsamrat24@gmail.com>
2021-04-01 10:33:55 +0600

    mongodb Connected Successfully

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+console.log(process.env.DB_USER)
 
 app.get('/', (req, res) => {
   res.send('Hi, I am Samrat!')
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpdhc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log('Connection Error', err)
+  const collection = client.db("dukanghorShop").collection("products");
+//   client.close();
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
