e935e389200cdb19e0cb254306753ee2cb276708
S.m.Samrat <imsamrat24@gmail.com>
2021-04-01 14:00:03 +0600

    Connect Database Successfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
@@ -21,8 +22,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     console.log('Connection Error', err)
-  const collection = client.db("dukanghorShop").collection("products");
-//   client.close();
+  const productCollection = client.db("dukanghorShop").collection("products");
+
+  app.get('/products', (req, res) => {
+      productCollection.find()
+      .toArray((err, items) =>{
+          res.send(items)
+      })
+  })
+  
+  
+  app.post('/addProduct', (req, res) => {
+      const newProduct = req.body;
+      console.log('Adding New Product', newProduct);
+      productCollection.insertOne(newProduct)
+      .then(result => {
+          console.log('inserted Count',result.insertedCount);
+          res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.delete('deleteProduct/:id', (req, res) =>{
+      const id = ObjectID(req.params.id)
+      console.log('delete this', id);
+      productCollection.findOneAndDelete({_id: id})
+      .then(documents => res.send(!!documents.value))
+  })
+
+  //   client.close();
 });
 
 
